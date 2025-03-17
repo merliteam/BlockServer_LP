6a974acdf1d54c5510ae5dffd357fb9de493b4e7
nicolas <nicolas>
2025-03-17 09:44:19 -0300

    boton de descargar modificado, fondo gris (bloqueado)

--- src/Components/BotonDescargar/index.tsx
+++ src/Components/BotonDescargar/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function BotonDescargar(){
     return(
-        <Link href={"/comenzar"}>
-            <button className=" px-5 py-2 bg-gradient-to-r from-purpleStart to-purpleEnd rounded-md font-semibold text-white transition-all hover:from-[#683ec2] hover:to-[#8f6edc]">
+        <Link href={"/auth"}>
+        <button className=" px-5 py-2 bg-gradient-to-r from-slate-500 to-slate-800 rounded-md font-semibold text-white transition-all hover:from-slate-700 hover:to-slate-950">
             Descargar
         </button>
         </Link>
