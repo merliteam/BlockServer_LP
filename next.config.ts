3e2585601c4f2704febdf89448d02a476f440301
javii42 <cjaviermollar@gmail.com>
2025-04-04 01:45:39 -0300

    IP BE - Cambiar por .env!!!

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ 
+  // ...
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Content-Security-Policy", value: "upgrade-insecure-requests" },
-          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-        ],
-      },
-    ];
   },
 };
 
